20480f781024b2bb8452b6482edd6491ad976d5d
Alen Iuri <alen.iuri@gmail.com>
2022-10-19 01:04:07 -0400

    -Deploy My Page

---
+++ app.py
@@ -0,0 +1,63 @@
+#! "C:\Users\ALEN IURI\lab\my-page\Scripts\python.exe"
+
+from flask import Flask, render_template, redirect, request, flash
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+from config import mail, password
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+SECRET_KEY = os.urandom(32)
+app.secret_key = SECRET_KEY
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.mailtrap.io',
+    "MAIL_PORT": 2525,
+    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": False,
+    "MAIL_USERNAME": mail,
+    "MAIL_PASSWORD": password
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+class Contact:
+    def __init__(self, nome, email, message):
+        self.nome = nome
+        self.email = email
+        self.message = message
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        contactForm = Contact(
+            request.form["nome"],
+            request.form["email"],
+            request.form["message"]
+        )
+
+        msg = Message(
+            subject = f'{contactForm.nome} lhe enviou uma mensagem no Portifólio!',
+            sender = app.config.get("MAIL_USERNAME"),
+            recipients= ['alen.iuri@gmail.com', app.config.get("MAIL_USERNAME")],
+            body = f'''
+            
+            {contactForm.nome} com e-mail <{contactForm.email}> lhe enviou a seguinte mensagem:
+            {contactForm.message}
+            '''
+        )
+
+        mail.send(msg)
+        flash('Mensagem enviada com sucesso!')
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,54 @@
+/* Open and close navbar <mode mobile> */
+
+const navbar = document.querySelector('.mobile-menu');
+const body = document.querySelector('body');
+
+navbar.addEventListener('click',() => {
+    navbar.classList.contains("bi-list")
+    ? navbar.classList.replace("bi-list", "bi-x")
+    : navbar.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("nav-menu-active")
+});
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("nav-menu-active")) {
+            body.classList.remove("nav-menu-active");
+            navbar.classList.replace("bi-x", "bi-list");
+        }
+    });
+});
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+            } else {
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
+
+const btnSubmit = document.querySelector('#btn-submit')
+const btnSubmitLoader = document.querySelector('#btn-submit-loader')
+
+btnSubmit.addEventListener("click", () => {
+    btnSubmitLoader.style.display = "block";
+    btnSubmit.style.display = "none"
+})
+
+setTimeout(() => {
+    document.querySelector('#alert').style.display = 'none';
+}, 5000)
